ce12d7c6e3a6ef01939d15770c67f4af9c5881fe
Gabriel Sancho <gabriel.sancho13@gmail.com>
2020-05-25 19:37:47 -0300

    .

--- testes.c
+++ testes.c
@@ -1,4 +1,45 @@
-a + b / strcmp(a && !(b + a)) || a((((((c * i)))))))
+#include <stdio.h>
+#include <stdlib.h>
+
+// Arquivo de teste
+
+int main(int argc, char *argv[])
 {
-	a = i;
+
+	int i = 0;
+	int j = 0;
+	int tam = 10;
+	int *array;
+
+	scanf("%m[^\n]s%c*", &i);
+	i = i * 2;
+	printf("\n");
+
+	array = malloc(sizeof(int) * tam);
+
+	for (j = 0; j < 10; j++)
+	{
+		if (j + 1 * (((i + j) + 2) / 3) % 2 && 1 || 3)
+		{
+			array[j] = j + i;
+		}
+	}
+
+	i = 0;
+
+	while (i < tam)
+	{
+		printf("%d\n", array[i]);
+		i++;
+	}
+
+	i = tam - 1;
+	printf("\n");
+
+	// do
+	// {
+	// 	printf("%d\n", array[i] * 2);
+	// } while (i--);
+
+	return 0;
 }
